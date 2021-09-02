58818175b2683ed6235cbb2b5968528552af9a7f
Felipe Farias <fefarias97@gmail.com>
2021-09-02 15:45:04 -0300

    Resolução exercício sobrenome

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace sobrenome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome, sobrenome;
+            Console.WriteLine("Qual o seu nome? ");
+            nome = Console.ReadLine();
+
+            Console.WriteLine("Qual o seu sobrenome? ");
+            sobrenome = Console.ReadLine();
+
+            Console.WriteLine($"{nome} {sobrenome}");
+            Console.WriteLine($"{sobrenome.ToUpper()}, {nome}");
+        }
+    }
+}
